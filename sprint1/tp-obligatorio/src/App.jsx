e3b162402a68ebd421c7e87d57b06a3f00961ca3
psiconervio <psiconervio@gmail.com>
2025-02-06 10:17:52 -0300

    asdasd

--- sprint1/tp-obligatorio/src/App.jsx
+++ sprint1/tp-obligatorio/src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className='bg-blue-500 text-white p-4'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
