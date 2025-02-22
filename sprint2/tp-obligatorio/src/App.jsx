1e586b229c88c3930c8abd9ff0b9826f21bc5acb
psiconervio <psiconervio@gmail.com>
2025-02-22 15:00:30 -0300

    asdad

---
+++ sprint2/tp-obligatorio/src/App.jsx
@@ -0,0 +1,27 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import EjemploMotion from './components/EjemploMotion'
+
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Navbar />
+    <Header />
+    <EjemploMotion  />
+    <Main />
+    <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ sprint2/tp-obligatorio/src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+const Header = () => {
+
+  return (
+    <header className="bg-blue-900/40 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-bold">¡Hola mundo!</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
