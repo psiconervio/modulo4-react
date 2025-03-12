352aaeeced96fd704055be48ba99e3197e630be9
psiconervio <psiconervio@gmail.com>
2025-03-12 20:36:06 -0300

    asdasd

--- sprint3/tp/vite.config.js
+++ sprint3/tp/vite.config.js
@@ -5,10 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    assetsDir: 'assets',
-  },
-  base: '/tp/',
+  base: '/',
   plugins: [react(),
         tailwindcss(),
 
