4e0ba1cfda5cc47f604d2e38cc73592dce0fa5ad
psiconervio <psiconervio@gmail.com>
2025-03-13 21:31:16 -0300

    Agregar contexto de autenticación y proveedor en AuthContext.jsx

--- sprint3/tp/src/context/AuthContext.jsx
+++ sprint3/tp/src/context/AuthContext.jsx
@@ -2,3 +2,15 @@ import React, { createContext, useContext, useState } from 'react';
 // 1 crear contexto de autenticacion
 // 2 crear provider, que va a envolver la aplicacion y va a manejar el state global
 // 3 crear custom hook para poder usarlo mas facilmente
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
