ff11d02a6dd931c4fd72c056195deef60abd3aa3
psiconervio <psiconervio@gmail.com>
2025-03-13 21:28:27 -0300

    asdas

---
+++ sprint3/tp/src/context/AuthContext.jsx
@@ -0,0 +1,4 @@
+import React, { createContext, useContext, useState } from 'react';
+// 1 crear contexto de autenticacion
+// 2 crear provider, que va a envolver la aplicacion y va a manejar el state global
+// 3 crear custom hook para poder usarlo mas facilmente
