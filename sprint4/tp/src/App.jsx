c9cb5a5c30bf74d52714fad3344750dac70283fc
LaboratorioInnovacion <psiconervio@gmail.com>
2025-03-20 20:00:34 -0300

    dsada

---
+++ sprint4/tp/src/App.jsx
@@ -0,0 +1,70 @@
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import { useState } from "react";
+import WatchlistModal from "./components/WatchlistModal";
+import MovieList from "./components/MovieList";
+import { useEffect, useContext } from "react";
+import { useAuth } from "./context/AuthContext";
+import ProductList from "./components/ProductList";
+import { Cart } from "./components/Cart";
+
+function App() {
+  const { isAuthenticated } = useAuth();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [watchlist, setWatchlist] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [isModalOpenCart, setIsModalOpenCart] = useState(false);
+
+  //ver si hay peliculas en la watchlist en localstorage
+  useEffect(() => {
+    const storedWatchlist = localStorage.getItem("watchlist");
+    if (storedWatchlist) {
+      setWatchlist(JSON.parse(storedWatchlist)); // Convierte la cadena en un objeto/array
+    }
+  }, []);
+
+  //Cargar desde localstorage
+  useEffect(() => {
+    const storeCart = localStorage.getItem("cart");
+    if (storeCart) {
+      setCart(JSON.parse(storeCart));
+    }
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <Header
+        setIsModalOpen={setIsModalOpen}
+        setIsModalOpenCart={setIsModalOpenCart}
+      />
+      <ProductList />
+      <Cart
+        isModalOpenCart={isModalOpenCart}
+        setIsModalOpenCart={setIsModalOpenCart}
+        cart={cart}
+        setCart={setCart}
+      />
+      {/* {!isAuthenticated ? (
+          <p>isAuthenticated : false</p>
+        ) : (
+          <p> isAuthenticated : true </p>
+        )} */}
+      {/* <WatchlistModal
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+        watchlist={watchlist}
+        setWatchlist={setWatchlist}
+      /> */}
+      {/* <MovieList watchlist={watchlist} setWatchlist={setWatchlist} /> */}
+      {/* <EjemploMotion /> */}
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ sprint4/tp/src/components/Header.jsx
@@ -0,0 +1,63 @@
+import React, { useContext } from "react";
+import { ThemeProvider } from "../context/ThemeContext";
+import { AuthProvider, useAuth } from "../context/AuthContext";
+import { ThemeContext } from "../context/ThemeContext";
+const Header = ({ setIsModalOpen, setIsModalOpenCart }) => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  // extraer los valores de el contexto
+  const { isAuthenticated } = useAuth();
+  console.log(isAuthenticated);
+  //Funcion para manejar  setIsModalOpen y pasarle al boton
+  const onOpenModal = () => {
+    setIsModalOpen(true);
+  };
+  //Funcion para manejar  setIsModalOpenCart y pasarle al boton
+  const onOpenModalCart = () => {
+    setIsModalOpenCart(true);
+  };
+
+  return (
+    // <header className="bg-blue-900/40 text-white p-4">
+    <header
+      className={
+        theme === "dark"
+          ? "bg-gray-800 text-white p-4"
+          : "bg-gray-600 text-white p-4"
+      }
+    >
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-bold ">Productos</h1>
+        {/* <button
+          className="bg-green-500 text-white px-4 py-2"
+          onClick={onOpenModal}
+        >
+          ver watchlist
+        </button> */}
+        <div>
+          <button
+            className="mx-5 bg-green-500 text-white px-4 py-2"
+            onClick={onOpenModalCart}
+          >
+            <i className="bi bi-bag"></i>
+          </button>
+          {/* <button
+            className={
+              theme === "dark"
+                ? "bg-gray-900 text-white"
+                : "bg-white text-black"
+            }
+            onClick={toggleTheme}
+          >
+            {theme === "light" ? (
+              <i className="bi bi-toggle-off"></i>
+            ) : (
+              <i className="bi bi-toggle-on"></i>
+            )}
+          </button> */}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
