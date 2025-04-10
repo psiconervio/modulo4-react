cbd6476049dcb6bcbdb29324840aa1a1f94ce4f2
psiconervio <psiconervio@gmai.com>
2025-04-10 12:04:18 -0300

    asdasd

--- sprint5/src/services/jugadoresapi.js
+++ sprint5/src/services/jugadoresapi.js
@@ -13,3 +13,33 @@ export const featchPersonaName = async (id) => {
     const response = await axios.get(url);
     return response.data;
   };
+
+  
+export const apidbmongo = async (id) => {
+  const url = `https://nodofullstack-m3-0w08.onrender.com/api/heroes/${id}`;
+  const response = await axios.get(url);
+  return response.data;
+};
+
+
+// router.get('/heroes', obtenerTodosLosSuperheroesController);
+// router.get('/heroes/:id', obtenerSuperheroePorIdController);
+// router.get('/heroes/buscar/:atributo/:valor', buscarSuperheroesPorAtributoController);
+// router.get('/heroes/mayores-30', obtenerSuperheroesMayoresDe30Controller);
+// // ruta para el nuevo endpoint
+// router.get('/heroes/buscar/mayores', obtenerSuperheroesMayoresDe30NativoController);
+// //rutas NUEVAS ruta post con middleware
+// // router.post('/heroes',validarCamposSuperHeroe, crearHeroeController );
+// router.post('/heroes',validarHeroeEvalidator, crearHeroeController);
+// // router.post('/heroes', crearHeroeController );
+// // ejercicio 3
+// router.put('/heroes/nombre/:nombreSuperHeroe',validarHeroeEvalidator, actualizarHeroePorNombre);
+// // ejercicio 4
+// router.delete('/heroes/id/:id', borrarHeroePorId);
+// //PETICION HTTP DE DELETE A GET PARA PODER ELMINAR 
+// router.get('/heroes/id/:id', borrarHeroePorId);
+// //Actualizar por id
+// // router.post('/heroes/idput/:id', actualizarHeroePorId);
+// router.put('/heroes/idput/:id',validarHeroeEvalidator, actualizarHeroePorId);
+// // ejercicio 5 Endpoint DELETE para eliminar un heroe por nombre
+// router.delete('/heroes/nombre/:nombre', borrarHeroePorNombre);
