809e869d2ac2f32b9aa1c8be5efabdfa2887d06e
psiconervio <psiconervio@gmai.com>
2025-04-16 12:17:21 -0300

    asda

--- sprint5/src/pages/ItemDetail.jsx
+++ sprint5/src/pages/ItemDetail.jsx
@@ -1,23 +1,28 @@
-import { useContext,useEffect,useState } from "react";
-import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useParams, Navigate, useNavigate } from "react-router-dom";
 import { useItem } from "../context/ItemContext";
 
 const ItemDetail = () => {
-  const { getItem, heroesfav, setHeroesfav } = useItem();
+  const { heroesfav, items, setHeroesfav } = useItem();
   const { id } = useParams(); // Extrae el id de la URL
   console.log("id", id);
   const [loading, setLoading] = useState(true); // Estado para manejar la carga
   const [error, setError] = useState(null); // Estado para manejar errores
+  const [itemdetalle, setItemdetalle] = useState([]); // Estado para manejar los detalles del item
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         setLoading(true);
-        // Reemplaza esta URL con la de tu API que devuelve un objeto por ID
-        const response = await fetch(`https://nodofullstack-m3-0w08.onrender.com/api/heroes/id/${id}`);
+        const response = await fetch(
+          `https://nodofullstack-m3-0w08.onrender.com/api/heroes/id/${id}`
+        );
         if (!response.ok) throw new Error("No se pudo obtener los datos.");
         const data = await response.json();
-        setHeroesfav(data);
+
+        // Si la API devuelve un objeto, conviértelo en un array
+        setItemdetalle(Array.isArray(data) ? data : [data]);
       } catch (err) {
         setError(err.message);
       } finally {
@@ -32,13 +37,13 @@ const ItemDetail = () => {
   if (error) return <p>Error: {error}</p>;
 
   console.log("heroesfav", heroesfav);
+  console.log("itemsdetalle", itemdetalle);
 
   return (
     <>
-      <div>ItemDetail</div>
-      <button onClick={() => getItem()}> BOTONAPI</button>
-      <div>
-        {heroesfav.map((item) => (
+      <div className="mx-10 mt-5 w-50">
+      <h1>Detalle del héroe</h1>
+        {itemdetalle.map((item) => (
           <div key={item.id}>
             <h1>{item.nombreSuperHeroe}</h1>
             <h2>{item.nombreReal}</h2>
@@ -50,6 +55,7 @@ const ItemDetail = () => {
             <p>{item.enemigos}</p>
           </div>
         ))}
+        <button onClick={()=>navigate(`edit`)}>editar</button>
       </div>
     </>
   );
