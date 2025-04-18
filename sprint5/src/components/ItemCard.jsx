89eff0571748043ec5295f65574457a530cf8007
LaboratorioInnovacion <psiconervio@gmail.com>
2025-04-18 01:22:08 -0300

    asdad

--- sprint5/src/components/ItemCard.jsx
+++ sprint5/src/components/ItemCard.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const ItemCard = ({ id, nombreReal, nombreSuperHeroe, Edad }) => {
+const ItemCard = ({ id, nombreReal, nombreSuperHeroe, edad, Edad }) => {
   const navigate = useNavigate();
 
+  const deleteData = async () => {
+    try {
+      const response = await axios.delete(
+        `https://nodofullstack-m3-0w08.onrender.com/api/heroes/id/${id}`
+      );
+
+      if (response.status === 200) {
+        alert("Item eliminado exitosamente.");
+        navigate("/"); // Redirige a la lista de items después de eliminar
+      } else {
+        alert("Error al eliminar el item.");
+      }
+    } catch (error) {
+      console.error("Error al realizar la solicitud:", error);
+      alert("Ocurrió un error al intentar eliminar el item.");
+    }
+  };
+
+  // useEffect(() => {
+  //   console.log(id, nombreReal, nombreSuperHeroe, edad, Edad, "desde el item card");
+  // }, []);
   return (
     <div className=" text-white border border-gray-300 rounded-lg shadow-md p-4 hover:shadow-lg transition duration-200">
-      <h1 className="text-lg font-bold  mb-2">
-        NombreSuperheroe:{nombreSuperHeroe}
+      <h1 className="text-1xlg font-bold  mb-2">
+        Nombre Super heroe:{nombreSuperHeroe}
       </h1>
       <h2 className="text-sm mb-1">Nombre Real: {nombreReal}</h2>
-      <h3 className="text-sm mb-1">Edad: {Edad}</h3>
+      <h3 className="text-sm mb-1">Edad: {edad}</h3>
       <button
         onClick={() => navigate(`/items/${id}/edit`)}
         className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-200"
@@ -23,40 +45,14 @@ const ItemCard = ({ id, nombreReal, nombreSuperHeroe, Edad }) => {
       >
         Ver Detalles
       </button>
-    
+      <button
+        className="bg-red-600 px-4 py-2 rounded"
+        onClick={() => deleteData(id)}
+      >
+        borrar
+      </button>
     </div>
   );
 };
 
 export default ItemCard;
-// // ItemCard.jsx
-// import React, { useContext, useEffect, useState } from "react";
-// import { useParams, useNavigate } from "react-router-dom";
-// import { useItem } from "../context/ItemContext";
-
-// const ItemCard = (
-//   id = "",
-//   nombreReal = "",
-//   nombreSuperHeroe = "",
-//   edad = ""
-// ) => {
-//   const { heroesfav, items, setHeroesfav } = useItem();
-// //   const { id } = useParams(); // Extrae el id de la URL
-//   const [loading, setLoading] = useState(true); // Estado para manejar la carga
-//   const navigate = useNavigate();
-
-//   return (
-//     <div>
-//       <h1>Detalle del héroe</h1>
-//       <h1>{items.nombreSuperHeroe}</h1>
-//       <h2>{items.nombreReal}</h2>
-//       <h3>{items.edad}</h3>
-//       <h3>{items.id}</h3>
-//       {/* <h4>{planetaOrigen}</h4>
-//       <h5>{debilidad}</h5> */}
-//       <button onClick={() => navigate(`edit`)}>editar</button>
-//     </div>
-//   );
-// };
-
-// export default ItemCard;
